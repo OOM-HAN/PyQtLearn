60d72bae7a35df69474a30946356fd7d65e13292
HJB <2022653432@qq.com>
2022-04-17 21:46:41 +0800

    PyQt_first

---
+++ PyTest/cmake-build-debug/CMakeFiles/PyTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PyTest.dir/main.cpp.obj"
+  "PyTest.exe"
+  "PyTest.exe.manifest"
+  "PyTest.pdb"
+  "libPyTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PyTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PyTest/first.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(300, 300)
+    w.move(300, 300)
+    w.setWindowTitle('第一个PyQt5的桌面应用')
+    w.show()
+    sys.exit(app.exec_())
